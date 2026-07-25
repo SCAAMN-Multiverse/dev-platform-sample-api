5648596c27f4b246bf104202d1a2462092c0dd6f
fjcuenase <fjcuenase@gmail.com>
2026-07-25 23:35:54 +0200

    feat: marca de verificacion end-to-end del lazo GitOps

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'sample-api gitops-e2e-213554';
   }
 }
