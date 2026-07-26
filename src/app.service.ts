8a21c4865c79cfdf8d9033d0d9d5dd8a41c058cf
fjcuenase <fjcuenase@gmail.com>
2026-07-26 12:20:30 +0200

    chore: medicion del lazo con el runner de organizacion

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'sample-api gate-fase6';
+    return 'sample-api runner-org-1';
   }
 }
